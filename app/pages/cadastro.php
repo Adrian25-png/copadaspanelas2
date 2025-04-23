86c30fc70dbdba791fc5e01474e319ff780f3b65
EduardoCOliveira-tech <eduardocarvalhooliveira2@gmail.com>
2025-04-22 22:08:31 -0300

    Update cadastro.php

--- app/pages/cadastro.php
+++ app/pages/cadastro.php
@@ -15,18 +15,36 @@
     <div class="container">
         <h1>Cadastro</h1>
         <form class="register-form" method="POST" action="">
-            <input type="text" placeholder="Nome" required>
-            <input type="text" placeholder="Sobrenome" required>
-            <input type="text" placeholder="Nome de Usuário" required>
-            <input type="email" placeholder="Email" required>
-            <input type="password" placeholder="Senha" required>
-            <input type="password" placeholder="Confirmação de Senha" required>
-            
+            <div class="input-field">
+              <input type="text" name="nome" required spellcheck="false">
+              <label>Nome</label>
+            </div>
+            <div class="input-field">
+              <input type="text" name="sobrenome" required spellcheck="false">
+              <label>Sobrenome</label>
+            </div>
+            <div class="input-field">
+              <input type="text" name="usuario" required spellcheck="false">
+              <label>Nome de Usuário</label>
+            </div>
+            <div class="input-field">
+              <input type="email" name="email" required spellcheck="false">
+              <label>Email</label>
+            </div>
+            <div class="input-field">
+              <input type="password" name="senha" required>
+              <label>Senha</label>
+            </div>
+            <div class="input-field">
+              <input type="password" name="confirmar_senha" required>
+              <label>Confirmação de Senha</label>
+            </div>
             <button type="submit">REGISTRAR</button>
-        </form>
+          </form>
     </div>
 
     <?php include 'footer.php'; ?>
+
 </body>
 
 </html>
