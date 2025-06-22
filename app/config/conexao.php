d1d90791eda9f8c3094d9257fd9d6e899ed7c611
Adriantest123 <deividadrian123@gmail.com>
2025-06-21 23:58:16 -0300

    bora ver agora
    a

--- app/config/conexao.php
+++ app/config/conexao.php
@@ -1,10 +1,20 @@
 <?php
 function conectar() {
-    $host = 'localhost'; // ou '127.0.0.1'
-    $port = '3306';
-    $dbname = 'copa';
-    $username = 'root';
-    $password = ''; // sua senha local do MySQL, se tiver
+    if ($_SERVER['SERVER_NAME'] === 'localhost') {
+        // Ambiente local (XAMPP)
+        $host = '127.0.0.1';
+        $port = '3306';
+        $dbname = 'copa';
+        $username = 'root';
+        $password = ''; // senha local do XAMPP
+    } else {
+        // Ambiente Railway
+        $host = 'mysql.railway.internal';
+        $port = '3306';
+        $dbname = 'railway'; // troque aqui pelo nome real do banco no Railway
+        $username = 'root';
+        $password = 'awesBgGcSgDAJOgEEMIEVBvjuAvaXRJW'; // senha real do Railway
+    }
 
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8mb4";
 
